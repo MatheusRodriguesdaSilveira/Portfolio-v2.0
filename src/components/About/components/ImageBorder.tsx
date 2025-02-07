fdad2d5fb1be4ada4e79211ac364d5f928fdbc5b
Matheus Rodrigues da Silveira <matheussilveira2005@gmail.com>
2025-02-06 23:24:47 -0300

    add: grid skills | fix grid responsive

--- src/components/About/components/ImageBorder.tsx
+++ src/components/About/components/ImageBorder.tsx
@@ -10,43 +10,43 @@ export const ImageBorder = () => {
   return (
     <div className="relative h-full w-full flex items-center justify-center xl:mr-16 2xl:mr-20">
       <div className="profileCard_container relative p-5 xl:p-8 2xl:p-10 border-2 border-dashed rounded-full border-gray-400/50">
-        <button className="profile_item absolute left-[60px] top-[-10px] xl:left-[80px] xl:top-[-15px] 2xl:left-[100px] 2xl:top-[-15px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500">
+        <button className="profile_item absolute left-[60px] top-[-10px] xl:left-[80px] xl:top-[-15px] 2xl:left-[100px] 2xl:top-[-15px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500 hover:shadow-xl hover:shadow-blue-500/50">
           <span className="size-6 xl:size-8 2xl:size-10 rounded-full bg-zinc-700 flex items-center justify-center p-1">
             <FaReact className="size-6 text-blue-600" />
           </span>
         </button>
 
-        <button className="profile_item absolute right-[50px] top-[-5px] xl:right-[70px] xl:top-[-10px] 2xl:right-[70px] 2xl:top-[-5px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500">
+        <button className="profile_item absolute right-[50px] top-[-5px] xl:right-[70px] xl:top-[-10px] 2xl:right-[70px] 2xl:top-[-5px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500 hover:shadow-xl hover:shadow-red-500/50">
           <span className="size-6 xl:size-8 2xl:size-10 rounded-full bg-zinc-700 flex items-center justify-center p-1">
             <Coffee className="size-6 text-rose-600" />
           </span>
         </button>
 
-        <button className="profile_item absolute left-[-15px] top-[70px] xl:left-[-20px] xl:top-[100px] 2xl:left-[-15px] 2xl:top-[100px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500">
+        <button className="profile_item absolute left-[-15px] top-[70px] xl:left-[-20px] xl:top-[100px] 2xl:left-[-15px] 2xl:top-[100px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500 hover:shadow-xl hover:shadow-green-500/50">
           <span className="size-6 xl:size-8 2xl:size-10 rounded-full bg-zinc-700 flex items-center justify-center p-1">
             <Braces className="size-6 text-lime-600" />
           </span>
         </button>
 
-        <button className="profile_item absolute right-[-15px] top-[70px] xl:right-[-20px] xl:top-[100px] 2xl:right-[-15px] 2xl:top-[100px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500">
+        <button className="profile_item absolute right-[-15px] top-[70px] xl:right-[-20px] xl:top-[100px] 2xl:right-[-15px] 2xl:top-[100px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500 hover:shadow-xl hover:shadow-yellow-500/50">
           <span className="size-6 xl:size-8 2xl:size-10 rounded-full bg-zinc-700 flex items-center justify-center p-1">
             <RiJavascriptFill className="size-6 text-yellow-300" />
           </span>
         </button>
 
-        <button className="profile_item absolute left-[0] bottom-[40px] xl:left-[0] xl:bottom-[50px] 2xl:left-[0] 2xl:bottom-[60px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500">
+        <button className="profile_item absolute left-[0] bottom-[40px] xl:left-[0] xl:bottom-[50px] 2xl:left-[0] 2xl:bottom-[60px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500 hover:shadow-xl hover:shadow-orange-500/50">
           <span className="size-6 xl:size-8 2xl:size-10 rounded-full bg-zinc-700 flex items-center justify-center p-1">
             <FaGitAlt className="size-6 text-orange-600" />
           </span>
         </button>
 
-        <button className="profile_item absolute right-[0] bottom-[40px] xl:right-[0] xl:bottom-[50px] 2xl:right-[0] 2xl:bottom-[60px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500">
+        <button className="profile_item absolute right-[0] bottom-[40px] xl:right-[0] xl:bottom-[50px] 2xl:right-[0] 2xl:bottom-[60px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500 hover:shadow-xl hover:shadow-lime-500/50">
           <span className="size-6 xl:size-8 2xl:size-10 rounded-full bg-zinc-700 flex items-center justify-center p-1">
             <FaNodeJs className="size-6 text-green-500" />
           </span>
         </button>
 
-        <button className="profile_item absolute right-[40%] bottom-[-15px] xl:right-[40%] xl:bottom-[-25px] 2xl:right-[40%] 2xl:bottom-[-30px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500">
+        <button className="profile_item absolute right-[40%] bottom-[-15px] xl:right-[40%] xl:bottom-[-25px] 2xl:right-[40%] 2xl:bottom-[-30px] rounded-full bg-cover cursor-pointer border border-gray-400/50 p-1 xl:p-2 active:scale-95 hover:scale-95 transition-all duration-500 hover:shadow-xl hover:shadow-blue-500/50">
           <span className="size-6 xl:size-8 2xl:size-10 rounded-full bg-zinc-700 flex items-center justify-center p-1">
             <BiLogoPostgresql className="size-6 text-sky-500" />
           </span>
@@ -57,7 +57,7 @@ export const ImageBorder = () => {
             <Image
               src={Me}
               alt="Me"
-              className="p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 brightness-90 shadow-2xl shadow-indigo-500/50"
+              className="p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 brightness-90 shadow-2xl shadow-indigo-500/50 hover:shadow-blue-500/70 duration-300"
             />
           </div>
         </button>
--- src/components/About/components/Techs.tsx
+++ src/components/About/components/Techs.tsx
@@ -10,7 +10,14 @@ import {
   Settings,
   Zap,
 } from "lucide-react";
-import { FaReact, FaHtml5, FaGithub, FaGitAlt } from "react-icons/fa";
+import {
+  FaReact,
+  FaHtml5,
+  FaGithub,
+  FaGitAlt,
+  FaCss3,
+  FaCss3Alt,
+} from "react-icons/fa";
 import {
   SiJavascript,
   SiTailwindcss,
@@ -22,6 +29,8 @@ import {
   SiExpress,
   SiInsomnia,
   SiBeekeeperstudio,
+  SiCss3,
+  SiCsswizardry,
 } from "react-icons/si";
 import { PiFileSqlBold } from "react-icons/pi";
 
@@ -38,35 +47,39 @@ export function ActionsGrid() {
           </span>
         </h2>
       </BlurFade>
-      <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center xl:gap-2 2xl:gap-5 mb-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center gap-5 xl:gap-2 2xl:gap-5 mb-20">
         {/* Front-end */}
         <BlurFade delay={0.25 * 4} inView>
-          <div className="flex flex-col gap-2 items-center border border-zinc-500 rounded-xl xl:px-24 py-10 xl:py-9 2xl:py-14 shadow-lg shadow-indigo-500/40 hover:scale-95 hover:shadow-indigo-500/70 duration-500">
+          <div className="group flex flex-col gap-2 items-center border border-zinc-500 rounded-xl xl:px-24 py-10 xl:py-9 2xl:py-14 shadow-lg shadow-indigo-500/40 hover:scale-95 hover:shadow-indigo-500/80 hover:-translate-y-2 duration-300 ease-in-out">
             <div className="flex items-center justify-center gap-1 mb-5">
-              <Zap className="size-7 xl:size-8 text-sky-400" />
+              <Zap className="size-7 xl:size-8 text-sky-400 transition-transform duration-300 ease-in-out group-hover:rotate-[20deg] group-hover:scale-110" />
               <span className="text-xl md:text-3xl font-semibold text-white">
                 Front-end
               </span>
             </div>
-            <div className="grid grid-flow-col grid-rows-1 md:grid-rows-1 px-5 gap-2 xl:gap-4">
+            <div className="grid grid-flow-col grid-rows-2 md:grid-rows-1 px-20 gap-5 xl:gap-4">
               <div className="flex flex-col items-center gap-1">
-                <SiJavascript className="size-9 xl:size-10 text-yellow-500" />
+                <SiJavascript className="size-9 xl:size-10 text-yellow-500 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">JavaScript</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <FaReact className="size-9 xl:size-10 text-blue-700" />
+                <FaReact className="size-9 xl:size-10 text-blue-700 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">React</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <SiNextdotjs className="size-9 xl:size-10 text-white" />
+                <SiNextdotjs className="size-9 xl:size-10 text-white transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">Next.Js</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <FaHtml5 className="size-9 xl:size-10 text-orange-500" />
+                <FaHtml5 className="size-9 xl:size-10 text-orange-500 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">Html</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <SiTailwindcss className="size-9 xl:size-10 text-sky-400" />
+                <FaCss3Alt className="size-9 xl:size-10 text-blue-500 transition-transform duration-300 group-hover:scale-110" />
+                <p className="text-white text-xs font-semibold">Css</p>
+              </div>
+              <div className="flex flex-col items-center gap-1">
+                <SiTailwindcss className="size-9 xl:size-10 text-sky-400 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">TailwindCss</p>
               </div>
             </div>
@@ -75,36 +88,36 @@ export function ActionsGrid() {
 
         {/* Back-end */}
         <BlurFade delay={0.25 * 5} inView>
-          <div className="flex flex-col gap-2 items-center border border-zinc-500 rounded-xl xl:px-24 py-10 xl:py-9 2xl:py-14 shadow-lg shadow-indigo-500/40 hover:scale-95 hover:shadow-indigo-500/70 duration-500">
+          <div className="group flex flex-col gap-2 items-center border border-zinc-500 rounded-xl xl:px-24 py-10 xl:py-9 2xl:py-14 shadow-lg shadow-indigo-500/40 hover:scale-95 hover:shadow-indigo-500/80 hover:-translate-y-2 duration-300 ease-in-out">
             <div className="flex items-center justify-center gap-1 mb-5">
-              <Rocket className="size-7 xl:size-8 text-rose-700" />
+              <Rocket className="size-7 xl:size-8 text-rose-700 transition-transform duration-300 ease-in-out group-hover:rotate-[20deg] group-hover:scale-110" />
               <span className="text-xl md:text-3xl font-semibold text-white">
                 Back-end
               </span>
             </div>
             <div className="grid grid-flow-col grid-rows-2 md:grid-rows-1 gap-5 xl:gap-4">
               <div className="flex flex-col items-center gap-1">
-                <SiNodedotjs className="size-9 xl:size-10 text-green-500" />
+                <SiNodedotjs className="size-9 xl:size-10 text-green-500 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">Node.Js</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <SiExpress className="size-9 xl:size-10 text-lime-600" />
+                <SiExpress className="size-9 xl:size-10 text-lime-600 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">Express</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <SiTypescript className="size-9 xl:size-10 text-blue-600" />
+                <SiTypescript className="size-9 xl:size-10 text-blue-600 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">TypeScript</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <SiPrisma className="size-9 xl:size-10 text-cyan-900" />
+                <SiPrisma className="size-9 xl:size-10 text-cyan-900 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">Prisma</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <PiFileSqlBold className="size-9 xl:size-10 text-sky-500" />
+                <PiFileSqlBold className="size-9 xl:size-10 text-sky-500 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">SQL</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <SiPostgresql className="size-9 xl:size-10 text-sky-500" />
+                <SiPostgresql className="size-9 xl:size-10 text-sky-500 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">PostgreSQL</p>
               </div>
             </div>
@@ -113,33 +126,36 @@ export function ActionsGrid() {
 
         {/* Other Technologies*/}
         <BlurFade delay={0.25 * 6} inView>
-          <div className="flex flex-col gap-2 items-center border border-zinc-500 rounded-xl xl:px-24 py-10 xl:py-9 2xl:py-14 shadow-lg shadow-indigo-500/40 hover:scale-95 hover:shadow-indigo-500/70 duration-500">
+          <div className="group flex flex-col gap-2 items-center border border-zinc-500 rounded-xl xl:px-24 py-10 xl:py-9 2xl:py-14 shadow-lg shadow-indigo-500/40 hover:scale-95 hover:shadow-indigo-500/80 hover:-translate-y-2 duration-300 ease-in-out">
             <h2 className="md:text-3xl font-semibold text-white flex items-center justify-center gap-2 mb-5">
-              <Settings className="size-7 text-pink-500" /> Other Technologies
+              <Settings className="size-7 text-pink-500 transition-transform duration-300 group-hover:rotate-12" />
+              <span className="text-xl md:text-3xl font-semibold text-white">
+                Other Technologies
+              </span>
             </h2>
             <div className="grid grid-flow-col grid-rows-2 md:grid-rows-1 gap-5 xl:gap-4">
               <div className="flex flex-col items-center gap-1">
-                <FaGithub className="size-8 xl:size-10 text-purple-500" />
+                <FaGithub className="size-9 xl:size-10 text-purple-500 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">GitHub</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <FaGitAlt className="size-8 xl:size-10 text-orange-400" />
+                <FaGitAlt className="size-9 xl:size-10 text-orange-400 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">Git</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <Figma className="size-8 xl:size-10 text-pink-500" />
+                <Figma className="size-9 xl:size-10 text-pink-500 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">Figma</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <VscVscode className="size-8 xl:size-10 text-blue-500" />
+                <VscVscode className="size-9 xl:size-10 text-blue-500 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">VsCode</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <SiInsomnia className="size-8 xl:size-10 text-indigo-700" />
+                <SiInsomnia className="size-9 xl:size-10 text-indigo-700 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">Insomnia</p>
               </div>
               <div className="flex flex-col items-center gap-1">
-                <SiBeekeeperstudio className="size-8 xl:size-10 text-yellow-300" />
+                <SiBeekeeperstudio className="size-9 xl:size-10 text-yellow-300 transition-transform duration-300 group-hover:scale-110" />
                 <p className="text-white text-xs font-semibold">Beekeeper</p>
               </div>
             </div>
@@ -148,9 +164,9 @@ export function ActionsGrid() {
 
         {/* Soft Skills*/}
         <BlurFade delay={0.25 * 7} inView>
-          <div className="flex flex-col gap-2 items-center border border-zinc-500 rounded-xl py-10 xl:px-5 xl:py-4 2xl:py-14 shadow-lg shadow-indigo-500/40 hover:scale-95 hover:shadow-indigo-500/70 duration-500">
+          <div className="group flex flex-col gap-2 items-center border border-zinc-500 rounded-xl py-10 xl:px-5 xl:py-4 2xl:py-14 shadow-lg shadow-indigo-500/40 hover:scale-95 hover:shadow-indigo-500/80 hover:-translate-y-2 duration-300 ease-in-out">
             <h2 className="text-lg xl:text-3xl 2xl:mb-1 font-semibold text-white flex items-center justify-center gap-1">
-              <BadgeCheck className="size-6 xl:size-7 text-yellow-300" />
+              <BadgeCheck className="size-6 xl:size-7 text-yellow-300 transition-transform duration-300 group-hover:rotate-12" />
               Soft Skills
             </h2>
             <div className="grid grid-flow-col grid-rows-10 md:grid-rows-3 gap-1 px-5 xl:px-0 xl:gap-0 2xl:gap-2 text-xs xl:text-sm text-white font-bold text-balance md:text-balance">
