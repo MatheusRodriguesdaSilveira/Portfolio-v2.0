57d5a95bb6bb2453c9f52115327a91f9841c6f37
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2026-01-24 17:54:05 -0300

    feat: update Contact component styles to use cyan color scheme and improve layout

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -17,7 +17,7 @@ const ContactPage = () => {
           "service_v6fo0xz",
           "template_wfs56lk",
           form.current!,
-          "9-ZvQjrAuHrx21zdA"
+          "9-ZvQjrAuHrx21zdA",
         )
         .then(
           () => {
@@ -30,7 +30,7 @@ const ContactPage = () => {
           },
           (error) => {
             console.error("Falha ao enviar o e-mail:", error);
-          }
+          },
         );
     }
   };
@@ -38,12 +38,12 @@ const ContactPage = () => {
   return (
     <BlurFade delay={0.25 * 5} inView>
       <div className="h-auto xl:min-h-screen flex items-center justify-center m-10">
-        <div className="border border-yellow-300 p-10 rounded-3xl shadow-yellow-700/50 shadow-2xl max-w-md xl:max-w-xl xl:h-[500px] 2xl:h-[600px] w-full">
+        <div className="border border-cyan-300 p-10 rounded-3xl shadow-cyan-700/50 shadow-2xl max-w-md xl:max-w-xl xl:h-[500px] 2xl:h-[600px] w-full">
           <div className="text-center 2xl:mb-12 xl:mb-0 ">
             <div className="flex items-center gap-1 justify-center">
-              <CircleUser className="text-yellow-300 size-6 xl:size-12" />
+              <CircleUser className="text-cyan-300 size-6 xl:size-12" />
               <h2 className="text-zinc-300 text-2xl md:text-5xl">
-                Contact <span className="text-yellow-400">me.</span>
+                Contact <span className="text-cyan-400">me.</span>
               </h2>
             </div>
           </div>
@@ -57,34 +57,34 @@ const ContactPage = () => {
 
           <form ref={form} onSubmit={sendEmail} className="space-y-2">
             <div>
-              <label className="block text-sm font-medium text-yellow-300">
+              <label className="block text-sm font-medium text-cyan-300">
                 Nome
               </label>
               <input
                 type="text"
                 name="name"
-                className="mt-1 p-2 w-full text-white bg-transparent border border-yellow-400 rounded-md focus:ring-yellow-300 focus:border-yellow-300"
+                className="mt-1 p-2 w-full text-white bg-transparent border border-cyan-400 rounded-md focus:ring-cyan-300 focus:border-cyan-300"
                 placeholder="Deixe seu nome aqui..."
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-yellow-300">
+              <label className="block text-sm font-medium text-cyan-300">
                 Email
               </label>
               <input
                 type="email"
                 name="email"
-                className="mt-1 p-2 w-full text-white bg-transparent border border-yellow-400 rounded-md focus:ring-yellow-300 focus:border-yellow-300"
+                className="mt-1 p-2 w-full text-white bg-transparent border border-cyan-400 rounded-md focus:ring-cyan-300 focus:border-cyan-300"
                 placeholder="Insira o seu email aqui.."
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-yellow-300">
+              <label className="block text-sm font-medium text-cyan-300">
                 Mensagem
               </label>
               <textarea
                 name="message"
-                className="h-[100px] xl:h-[90px] 2xl:h-[120px] mt-1 p-2 w-full text-white bg-transparent border border-yellow-400 rounded-md focus:ring-yellow-300 focus:border-yellow-300"
+                className="h-[100px] xl:h-[90px] 2xl:h-[120px] mt-1 p-2 w-full text-white bg-transparent border border-cyan-400 rounded-md focus:ring-cyan-300 focus:border-cyan-300"
                 placeholder="Como posso te ajudar?"
               ></textarea>
             </div>
